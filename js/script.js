e4bef97fcfa360bb1b87438639e398a281264822
Alice-Hokage <AliceFedoseeva@yandex.ru>
2020-06-18 20:00:29 +0300

    some functionality on js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
+//п е р е м е н н ы е 
 const navBtn = document.querySelector('.top-nav_btn'),
-      navMenu = document.querySelector('.top-nav_menu');
-const sideBtn = document.querySelector('.left-sidebar_btn'),
-      sideMenu = document.querySelector('.left-sidebar_menu');
+      navMenu = document.querySelector('.top-nav_menu'),
+      sideBtn = document.querySelector('.left-sidebar_btn'),
+      sideMenu = document.querySelector('.left-sidebar_menu'),
+      links = document.querySelectorAll('a[href*="#"]'),
+      toBoth = document.querySelector('.to-both'),
+      toTop = document.querySelector('.to-top'),
+      openModal = document.querySelector('.object-card_btn'),
+      modal = document.querySelector('.modal');
 
-  sideBtn.addEventListener('click', function() {
-  sideMenu.classList.toggle('left-sidebar_menu__active')
-  });
 
-  navBtn.addEventListener('click', function() {
-  navMenu.classList.toggle('top-nav_menu__active')
-});
-const links = document.querySelectorAll('a[href*="#footer"]');
+      //плавная прокрутка для кнопочек
 for (let link of links) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
@@ -20,4 +20,36 @@ for (let link of links) {
       block: "start"
     })
   })
-}
+}; 
+//механика кнопочек up and down
+window.addEventListener('scroll', () => {
+  if(window.pageYOffset != 0) {
+    toBoth.style.display = 'none';
+    toTop.style.display = 'block';
+  }
+  else {
+    toBoth.style.display = 'block';
+    toTop.style.display = 'none';
+  }
+});
+//open and close madal window
+openModal.addEventListener('click', event => {
+  modal.style.display = 'block';
+  event.preventDefault();
+});
+modal.addEventListener('click', event => {
+  if (event.target.closest('.close') ||
+      event.target.classList.contains('modal') ||
+      event.target.closest('.modal-form_btn')) {
+      modal.style.display = 'none';
+    }
+  event.preventDefault();
+});
+//media event
+sideBtn.addEventListener('click', function() {
+  sideMenu.classList.toggle('left-sidebar_menu__active')
+});
+
+navBtn.addEventListener('click', function() {
+  navMenu.classList.toggle('top-nav_menu__active')
+});
