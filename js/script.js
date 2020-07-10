29aab1f85b5f70abc41dca9d4dbdb8cbd79ddebd
Alice-Hokage <AliceFedoseeva@yandex.ru>
2020-07-10 22:56:01 +0300

    render list menu

--- js/script.js
+++ js/script.js
@@ -3,53 +3,96 @@ const navBtn = document.querySelector('.top-nav_btn'),
       navMenu = document.querySelector('.top-nav_menu'),
       sideBtn = document.querySelector('.left-sidebar_btn'),
       sideMenu = document.querySelector('.left-sidebar_menu'),
-      links = document.querySelectorAll('a[href*="#"]'),
       toBoth = document.querySelector('.to-both'),
       toTop = document.querySelector('.to-top'),
-      openModal = document.querySelector('.object-card_btn'),
-      modal = document.querySelector('.modal');
+      content = document.querySelector('.content'),
+      modal = document.querySelector('.modal'),
+      category = document.querySelector('.category'),
+      titlePage = document.querySelector('.title-page');
 
-
-      //плавная прокрутка для кнопочек
-for (let link of links) {
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-    const blockID = link.getAttribute('href')
-    document.querySelector('' + blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    })
-  })
-}; 
-//механика кнопочек up and down
-window.addEventListener('scroll', () => {
-  if(window.pageYOffset != 0) {
-    toBoth.style.display = 'none';
+const getData = async function(url) {
+  const response = await fetch(url);
+  
+  if(!response.ok) {
+    throw new Error (`Ошибка по адресу ${url},
+                      ошибка ${response.status}!`);
+  }
+  return await response.json();
+}
+//механика кнопочки up
+const smoothScroll = () => {
+  if(window.pageYOffset != 0 && 
+    document.body.clientWidth > 650) {
     toTop.style.display = 'block';
   }
   else {
-    toBoth.style.display = 'block';
     toTop.style.display = 'none';
   }
-});
-//open and close madal window
-openModal.addEventListener('click', event => {
-  modal.style.display = 'block';
+}
+//плавная прокрутка для кнопочки
+const scrollToTop = (event) => {
   event.preventDefault();
-});
-modal.addEventListener('click', event => {
-  if (event.target.closest('.close') ||
-      event.target.classList.contains('modal') ||
-      event.target.closest('.modal-form_btn')) {
-      modal.style.display = 'none';
+  document.querySelector('body').scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  })
+}; 
+//open and close madal window
+const openModal = (event) => {
+  const target = event.target;
+  if (target.closest('.object-card_btn')) {
+      modal.classList.remove('hide');
+  }
+};
+const closeModal = (event) => {
+  const target = event.target;
+
+  if (target.closest('.close') ||
+      target.classList.contains('modal') ||
+      target.closest('.modal-form_btn')) {
+      modal.classList.add('hide');
     }
   event.preventDefault();
-});
-//media event
-sideBtn.addEventListener('click', function() {
-  sideMenu.classList.toggle('left-sidebar_menu__active')
-});
+};
+/*window.addEventListener('keydown', event => {
+  if (event.code == 'Escape') {
+      modal.classList.add('hide');
+  }
+}); */
+const createCategoryList = (category) => {
+  const {
+    name,
+    products
+  } = category;
+  const list = `
+          <li><a href="#">${name}</a></li>
+  `;
+sideMenu.insertAdjacentHTML('beforeend', list);
+};
 
-navBtn.addEventListener('click', function() {
-  navMenu.classList.toggle('top-nav_menu__active')
-});
+const createItemList = (list) {
+
+};
+function init() {
+
+  getData('./db/category.json').then(function(data) {
+    data.forEach(createCategoryList);
+  });
+
+  //media event
+  sideBtn.addEventListener('click', function() {
+    sideMenu.classList.toggle('left-sidebar_menu__active')
+  });
+  navBtn.addEventListener('click', function() {
+    navMenu.classList.toggle('top-nav_menu__active')
+  });
+  //плавный скрол
+  window.addEventListener('scroll', smoothScroll);
+  toTop.addEventListener('click', scrollToTop);
+
+  //open and close madal window
+  content.addEventListener('click', openModal);
+  modal.addEventListener('click', closeModal);
+
+};
+init();
