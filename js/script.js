3404322ee3ed52a503b1a487bbaea25d791cb6a4
Alice-Hokage <AliceFedoseeva@yandex.ru>
2020-07-12 22:58:05 +0300

    experental method create page

--- js/script.js
+++ js/script.js
@@ -11,10 +11,12 @@ const navBtn = document.querySelector('.top-nav_btn'),
       titlePage = document.querySelector('.title-page'),
       card = document.querySelector('.card'),
       itemListPage = document.querySelector('.item-list'),
-      logo = document.querySelector('.logo');
+      topNav = document.querySelector('.top-nav'),
+      footerMenu = document.querySelector('.footer_menu'),
+      footerMenuCategory = document.querySelector('.footer_menu-category');
 
 const getData = async function(url) {
-  const response = await fetch(url);
+  const response = await fetch(url); 
   
   if(!response.ok) {
     throw new Error (`Ошибка по адресу ${url},
@@ -71,8 +73,8 @@ const createCategoryList = category => {
           <li class="list" data-products ='${products}' data-name='${[name]}'><a>${name}</a></li>
   `;
 sideMenu.insertAdjacentHTML('beforeend', list);
+footerMenuCategory.insertAdjacentHTML('beforeend', list);
 };
-
 const createItemList = (item) => {
   const { id, title, description, price } = item;
 
@@ -101,32 +103,121 @@ const openCatalogItem = event => {
       content.classList.add('hide');
       itemListPage.classList.remove('hide');
       titlePage.textContent = name;
+      location.hash = '';
 
-      getData(`./db/${itemList.dataset.products}`).then(function(data) {
+      getData(`./db/category-db/${itemList.dataset.products}`).then(function(data) {
         data.forEach(createItemList);
   });
 }
 };
-const returnMain = () => {
+const getContent = ( idPage, callback) => {
+  const pages = {
+    service: `<p>cepturi? Voluptatibus iste eos ex! Nobis ea deleniti accusamus maiores,
+    corporis sed culpa error repellat natus molestiae at excepturi, consequuntur rem voluptatem!
+    Temporibus, minima odio iste ipsam sequi magni,
+    iure deserunt praesentium sapiente blanditiis iusto velit dolorem numquam,
+    modi consectetur placeat ullam. Eos minus sapiente distinctio officiis!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Repellendus numquam quidem nam perferendis <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+  Repellendus numquam quidem nam perferendis facilis obcaecati nisi iusto fugit commodi quae,
+  magni esse accusamus in soluta cum ut tempore et totam facere!
+  Dignissimos consequatur sequi quidem ipsa culpa accusamus officiis expedita?
+  Asperiores labore laboriosam nulla enim, consectetur doloribus natus repellat,
+  illum facere ea nostrum excepturi? Voluptatibus iste eos ex! Nobis ea deleniti accusamus maiores,
+  corporis sed culpa error repellat natus molestiae at excepturi, consequuntur rem voluptatem!
+  Temporibus, minima odio iste ipsam sequi magni,
+  iure deserunt praesentium sapiente blanditiis iusto velit dolorem numquam,
+  modi consectetur placeat ullam. Eos minus sapiente distinctio officiis!</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Repellendus numquam quidem nam perferendis facilis obcaecati nisi iusto fugit commodi quae,
+    magni esse accusamus in soluta cum ut tempore et totam facere!
+    Dignissimos consequatur sequi quidem ipsa culpa accusamus officiis expedita?
+    Asperiores labore laboriosam nulla enim, consectetur doloribus natus repellat,
+    illum facere ea nostrum exfacilis obcaecati nisi iusto fugit commodi quae,
+      magni esse accusamus in soluta cum ut tempore et totam facere!
+      Dignissimos consequatur sequi quidem ipsa culpa accusamus officiis expedita?
+      Asperiores labore laboriosam nulla enim, consectetur doloribus natus repellat,
+      illum facere ea nostrum excepturi? Voluptatibus iste eos ex! Nobis ea deleniti accusamus maiores,
+      corporis sed culpa error repellat natus molestiae at excepturi, consequuntur rem voluptatem!
+      Temporibus, minima odio iste ipsam sequi magni,
+      iure deserunt praesentium sapiente blanditiis iusto velit dolorem numquam,
+      modi consectetur placeat ullam. Eos minus sapiente distinctio officiis!</p>`,
+    contacts: `<div class="contacts">
+    <p class="contacts-phone">
+      <img class="contacts-img" src="/img/phone-handset_icon-icons.com_48252.png" alt="phone">
+      8 (888) 888-88-88</p>
+    <p class="contscts-mail">
+      <img class="contacts-img" src="/img/letter_icon-icons.com_67753.svg" alt="mail">
+      emailAddress@to.fe</p>
+    <p class="contacts-address">Санкт-Петербург, Новгородская улица, дом 16</p>
+    <a href="/about.html" class="btn btn-primary contacts-btn">Задать вопрос</a>
+    <div class="contacts-social-links">
+      <ul>
+        <li><a href="#"><i class="icon-facebook"></i></a></li>
+        <li><a href="#"><i class="icon-instagram"></i></a></li>
+        <li><a href="#"><i class="icon-vkontakte"></i></a></li>
+      </ul>
+    </div>
+  </div>`,
+    about: `<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Repellendus numquam quidem nam perferendis facilis obcaecati nisi iusto fugit commodi quae,
+    magni esse accusamus in soluta cum ut tempore et totam facere!
+    Dignissimos consequatur sequi quidem ipsa culpa accusamus officiis expedita?
+    Asperiores labore laboriosam nulla enim, consectetur doloribus natus repellat,
+    illum facere ea nostrum excepturi? Voluptatibus iste eos ex! Nobis ea deleniti accusamus maiores,
+    corporis sed culpa error repellat natus molestiae at excepturi, consequuntur rem voluptatem!
+    Temporibus, minima odio iste ipsam sequi magni,
+    iure deserunt praesentium sapiente blanditiis iusto velit dolorem numquam,
+    modi consectetur placeat ullam. Eos minus sapiente distinctio officiis!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Repellendus numquam quidem nam perferendis facilis obcaecati nisi iusto fugit commodi quae,
+      magni esse accusamus in soluta cum ut tempore et totam facere!
+      Dignissimos consequatur sequi quidem ipsa culpa accusamus officiis expedita?
+      Asperiores labore laboriosam nulla enim, consectetur doloribus natus repellat,
+      illum facere ea nostrum excepturi? Voluptatibus iste eos ex! Nobis ea deleniti accusamus maiores,
+      corporis sed culpa error repellat natus molestiae at excepturi, consequuntur rem voluptatem!
+      Temporibus, minima odio iste ipsam sequi magni,
+      iure deserunt praesentium sapiente blanditiis iusto velit dolorem numquam,
+      modi consectetur placeat ullam. Eos minus sapiente distinctio officiis!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Repellendus numquam quidem nam perferendis facilis obcaecati nisi iusto fugit commodi quae,
+        magni esse accusamus in soluta cum ut tempore et totam facere!
+        Dignissimos consequatur sequi quidem ipsa culpa accusamus officiis expedita?
+        Asperiores labore laboriosam nulla enim, consectetur doloribus natus repellat,
+        illum facere ea nostrum excepturi? Voluptatibus iste eos ex! Nobis ea deleniti accusamus maiores,
+        corporis sed culpa error repellat natus molestiae at excepturi, consequuntur rem voluptatem!
+        Temporibus, minima odio iste ipsam sequi magni,
+        iure deserunt praesentium sapiente blanditiis iusto velit dolorem numquam,
+        modi consectetur placeat ullam. Eos minus sapiente distinctio officiis!
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Alias molestias culpa delectus quae deleniti possimus consequuntur ipsam!
+            Dolores exercitationem repudiandae deleniti,
+          soluta a eaque quaerat porro ratione, odio impedit quam.</p>`
+  };
+  callback(pages[idPage]);
+};
+if (!location.hash) {
+  location.hash = "";
+}
+const loadingContent = () => {
+  const idPage = location.hash.substr(1);
+
+  getContent(idPage, function(page) {
+    content.innerHTML = page;
+  })
+};
+const returnMain = event => {
+  const target = event.target;
+  if (target.closest('a'))
   itemListPage.classList.add('hide');
   content.classList.remove('hide');
-};
-const createMenu = item => {
-  const { title } = item;
-  const listMenu = `
-  <li><a>${title}</a></li>
-  `;
-  navMenu.insertAdjacentHTML('beforeend', listMenu);
+  titlePage.textContent = 'Главная';
 };
 function init() {
 
   getData('./db/category.json').then(function(data) {
     data.forEach(createCategoryList);
   });
-  getData('./db/menu.json').then(function(data) {
-    data.forEach(createMenu);
-  });
-
   //media event
   sideBtn.addEventListener('click', function() {
     sideMenu.classList.toggle('left-sidebar_menu__active')
@@ -143,7 +234,11 @@ function init() {
   modal.addEventListener('click', closeModal);
 
   sideMenu.addEventListener('click', openCatalogItem);
+  footerMenuCategory.addEventListener('click', openCatalogItem);
+  topNav.addEventListener('click', returnMain);
 
-  logo.addEventListener('click', returnMain);
+  loadingContent();
+
+  window.addEventListener('hashchange', loadingContent);
 };
 init();
